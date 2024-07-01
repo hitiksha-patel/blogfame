a475c44ab052b269c595601333c705ad0e802803
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-01 15:57:55 -0400

    code changes

--- accounts/helper.py
+++ accounts/helper.py
@@ -4,14 +4,14 @@ from django.conf import settings
 
 def send_verify_mail(email, token):
     subject = 'Do Not Reply: You have a new message!'
-    message = f'Hi, thank you for registering in BlogFame. Please click on this link to verify your email address: https://blogfame.herokuapp.com/verify/{token} '
+    message = f'Hi, thank you for registering in BlogFame. Please click on this link to verify your email address: http://localhost/verify/{token} '
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, from_email, recipient_list)
 
 def forgot_password_mail(email, token):
     subject = 'Do Not Reply: You have a new message!'
-    message = f'Hi, It seems you forgot your password. Please click on this link to change your password: https://blogfame.herokuapp.com/Change_password/{token} '
+    message = f'Hi, It seems you forgot your password. Please click on this link to change your password: http://localhost/Change_password/{token} '
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, from_email, recipient_list)
